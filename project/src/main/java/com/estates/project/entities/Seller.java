2c300371b61c7b090e133cfb6a57d3c33821c188
AGQA2024 <codesoftware2579@gmail.com>
2024-03-21 14:35:24 +0000

    Test commit

--- project/src/main/java/com/estates/project/entities/Seller.java
+++ project/src/main/java/com/estates/project/entities/Seller.java
@@ -2,11 +2,22 @@ package com.estates.project.entities;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
 public class Seller {
 
+//    Test push
     @Id
+//    @GeneratedValue
     private Integer sellerId;
+
+    private String sellerFirstName;
+    private String sellerLastName;
+    private String sellerAddress;
+    private String sellerPostcode;
+    private String sellerPhone;
+
+
 }
