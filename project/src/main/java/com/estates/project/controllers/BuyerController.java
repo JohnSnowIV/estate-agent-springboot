7ccae9744c7ae387566d4520304b774f92b27800
JohnSnowIV <cloudtraining842@protonmail.com>
2024-03-26 10:45:07 +0000

    minor tweaks

--- project/src/main/java/com/estates/project/controllers/BuyerController.java
+++ project/src/main/java/com/estates/project/controllers/BuyerController.java
@@ -1,6 +1,7 @@
 package com.estates.project.controllers;
 
 import com.estates.project.entities.Buyer;
+import com.estates.project.entities.Seller;
 import com.estates.project.services.BuyerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -36,6 +37,16 @@ public class BuyerController {
     @DeleteMapping("/buyer/{id}")
     public void deleteBuyer(@PathVariable Integer id) {
         this.buyerService.deleteBuyer(id);
+    }
+
+    @PatchMapping("/buyer/{id}")
+    public Buyer updateBuyer(@PathVariable Integer id,
+                               @RequestParam(required = false) String firstName,
+                               @RequestParam(required = false) String surname,
+                               @RequestParam(required = false) String address,
+                               @RequestParam(required = false) String postcode,
+                               @RequestParam(required = false) String phone){
+        return this.buyerService.updateBuyer(id,firstName,surname,address,postcode,phone);
     }
 }
 
