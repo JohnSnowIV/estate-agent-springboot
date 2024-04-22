0dd426445390b16fe958b60960a800e8cf52b579
purpleapplebear <purpleapplebear@yahoo.com>
2024-04-22 13:07:09 +0100

    cors test

--- project/src/main/java/com/estates/project/ProjectApplication.java
+++ project/src/main/java/com/estates/project/ProjectApplication.java
@@ -3,6 +3,8 @@ package com.estates.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,12 +14,19 @@ public class ProjectApplication {
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
+
+			@Bean
+			public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+				http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest()
+								.permitAll());
+				return http.build();
+			}
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:5173")
-						.allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE");
-						//.allowedHeaders("Access-Control-Request-Headers", "Access-Control-Request-Method", "X-Requested-With", "Access-Control-Allow-Headers");
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
+						.allowedHeaders("Access-Control-Request-Headers", "Access-Control-Request-Method", "X-Requested-With", "Access-Control-Allow-Headers");
 			}
 		};
 	}
